f14ee42eda9dddeb0671fda93aa8d930c3bbd53b
samjhana091 <dhami.samjhana12345@gmail.com>
2018-06-17 20:15:15 +0545

    add header and body component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+import Header from './Header.js'
+import Body from './body.js'
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>main page</h1>
+        
+        <Header />
+        <Body />
+
       </div>
     );
   }
---
+++ src/body.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class body extends Component {
+    render() {
+        return (
+            <div>
+                <h1>body</h1>
+                First name: <input type="text" name="FirstName" value="Mickey" / >
+                Last name: <input type="text" name="LastName" value="Mouse" / >
+                <img src="https://images.pexels.com/photos/259803/pexels-photo-259803.jpeg?cs=srgb&dl=adorable-animal-animal-photography-259803.jpg&fm=jpg" alt="Smiley face" height="100" width="142" / >
+
+
+            </div>
+        )
+    }
+}
+
+export default body;
